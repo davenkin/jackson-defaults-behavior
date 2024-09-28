6a5d921ac578b22d3180a96f1a1b12a37bd79a80
Teng Yun <davenkin@126.com>
2024-09-28 10:20:18 +0800

    refactor

--- src/test/java/davenkin/jackson/polymorphism/PolymorphismUsingDefaultTypingTest.java
+++ src/test/java/davenkin/jackson/polymorphism/PolymorphismUsingDefaultTypingTest.java
@@ -1,10 +1,5 @@
 package davenkin.jackson.polymorphism;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,103 +9,100 @@ import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PolymorphismUsingDefaultTypingTest {
-  @Test
-  public void should_use_default_typing_for_serialization_and_deserialization() throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    @Test
+    public void should_use_default_typing_NON_FINAL_AND_ENUMS_for_serialization_and_deserialization() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-    PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-        .allowIfBaseType("davenkin")
-        .allowIfBaseType("java")
-        .build();
-    objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfBaseType("davenkin")
+                .allowIfBaseType("java")
+                .build();
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL_AND_ENUMS, JsonTypeInfo.As.PROPERTY);
 
-    Admin admin = new Admin();
-    admin.name = "Andy";
-    admin.type = EmployeeType.ADMIN;
-    admin.level = 45;
+        Company company = createCompany();
 
-    Worker worker = new Worker();
-    worker.name = "Mike";
-    worker.type = EmployeeType.WORKER;
-    worker.location = "Shanghai";
+        String json = objectMapper.writeValueAsString(company);
+        System.out.println(json);
 
-    Company company = new Company();
-    company.employees = List.of(admin, worker);
-    company.starEmployee = worker;
-    company.gatekeeper = worker;
-    company.administrator = admin;
+        // with NON_FINAL_AND_ENUMS, it even works with Object.class as the top level class (Company) also has type info injected during serialization
+        Company deserializedCompany = (Company) objectMapper.readValue(json, Object.class);
+        assertEquals("Andy", deserializedCompany.employees.get(0).name);
+        assertEquals("Mike", deserializedCompany.employees.get(1).name);
+    }
 
-    String json = objectMapper.writeValueAsString(company);
-    System.out.println(json);
+    @Test
+    public void fail_due_to_OBJECT_AND_NON_CONCRETE_not_working_with_root_level_Company() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-    // with NON_FINAL_AND_ENUMS, it even works with Object.class as the top level class (Company) also has type info injected during serialization
-    Company deserializedCompany = (Company) objectMapper.readValue(json, Object.class);
-    assertEquals("Andy", deserializedCompany.employees.get(0).name);
-    assertEquals("Mike", deserializedCompany.employees.get(1).name);
-  }
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfBaseType("davenkin")
+                .allowIfBaseType("java")
+                .build();
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE, JsonTypeInfo.As.PROPERTY);
 
-  @Test
-  public void fail_due_to_OBJECT_AND_NON_CONCRETE_not_working_with_root_level_Company() throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        Company company = createCompany();
 
-    PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-        .allowIfBaseType("davenkin")
-        .allowIfBaseType("java")
-        .build();
-    objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE, JsonTypeInfo.As.PROPERTY);
+        String json = objectMapper.writeValueAsString(company);
+        System.out.println(json);
 
-    Admin admin = new Admin();
-    admin.name = "Andy";
-    admin.type = EmployeeType.ADMIN;
-    admin.level = 45;
+        // OBJECT_AND_NON_CONCRETE does not work for deserialize into Object.class,
+        // because the type info in not included for Company during serialization
+        assertThrows(InvalidTypeIdException.class, () -> objectMapper.readValue(json, Object.class));
 
-    Worker worker = new Worker();
-    worker.name = "Mike";
-    worker.type = EmployeeType.WORKER;
-    worker.location = "Shanghai";
+        // but is we specifically use Company.class, it works
+        Company deserializedCompany = objectMapper.readValue(json, Company.class);
+        assertEquals("Andy", deserializedCompany.employees.get(0).name);
+        assertEquals("Mike", deserializedCompany.employees.get(1).name);
+    }
 
-    Company company = new Company();
-    company.employees = List.of(admin, worker);
-    company.starEmployee = worker;
-    company.gatekeeper = worker;
-    company.administrator = admin;
+    private static Company createCompany() {
+        Admin admin = new Admin();
+        admin.name = "Andy";
+        admin.type = EmployeeType.ADMIN;
+        admin.level = 45;
 
-    String json = objectMapper.writeValueAsString(company);
-    System.out.println(json);
+        Worker worker = new Worker();
+        worker.name = "Mike";
+        worker.type = EmployeeType.WORKER;
+        worker.location = "Shanghai";
 
-    // OBJECT_AND_NON_CONCRETE does not work for deserialize into Object.class, because the type info in not included for Company during serialization
-    assertThrows(InvalidTypeIdException.class, () -> objectMapper.readValue(json, Object.class));
+        Company company = new Company();
+        company.employees = List.of(admin, worker);
+        company.starEmployee = worker;
+        company.gatekeeper = worker;
+        company.administrator = admin;
+        return company;
+    }
 
-    // but is we specifically use Company.class, it works
-    Company deserializedCompany = objectMapper.readValue(json, Company.class);
-    assertEquals("Andy", deserializedCompany.employees.get(0).name);
-    assertEquals("Mike", deserializedCompany.employees.get(1).name);
-  }
+    public static class Company {
+        public List<Employee> employees;
+        public Employee starEmployee;
+        public Admin administrator;
+        public Worker gatekeeper;
+    }
 
-  public static class Company {
-    public List<Employee> employees;
-    public Employee starEmployee;
-    public Admin administrator;
-    public Worker gatekeeper;
-  }
+    public static abstract class Employee {
+        public String name;
+        public EmployeeType type;
+    }
 
-  public static abstract class Employee {
-    public String name;
-    public EmployeeType type;
-  }
+    public static class Admin extends Employee {
+        public int level;
+    }
 
-  public static class Admin extends Employee {
-    public int level;
-  }
+    public final static class Worker extends Employee {
+        public String location;
+    }
 
-  public final static class Worker extends Employee {
-    public String location;
-  }
-
-  public enum EmployeeType {
-    ADMIN, WORKER
-  }
+    public enum EmployeeType {
+        ADMIN, WORKER
+    }
 }
--- src/test/java/davenkin/jackson/polymorphism/PolymorphismUsingJsonTypeInfoTest.java
+++ src/test/java/davenkin/jackson/polymorphism/PolymorphismUsingJsonTypeInfoTest.java
@@ -1,9 +1,5 @@
 package davenkin.jackson.polymorphism;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,64 +7,68 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PolymorphismUsingJsonTypeInfoTest {
-  @Test
-  public void should_use_json_type_info_for_polymorphism() throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    @Test
+    public void should_use_JsonTypeInfo_polymorphism() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-    Admin admin = new Admin();
-    admin.name = "Andy";
-    admin.type = EmployeeType.ADMIN;
-    admin.level = 45;
+        Admin admin = new Admin();
+        admin.name = "Andy";
+        admin.type = EmployeeType.ADMIN;
+        admin.level = 45;
 
-    Worker worker = new Worker();
-    worker.name = "Mike";
-    worker.type = EmployeeType.WORKER;
-    worker.location = "Shanghai";
+        Worker worker = new Worker();
+        worker.name = "Mike";
+        worker.type = EmployeeType.WORKER;
+        worker.location = "Shanghai";
 
-    Company company = new Company();
-    company.employees = List.of(admin, worker);
-    company.starEmployee = worker;
-    company.gatekeeper = worker;
-    company.administrator = admin;
+        Company company = new Company();
+        company.employees = List.of(admin, worker);
+        company.starEmployee = worker;
+        company.gatekeeper = worker;
+        company.administrator = admin;
 
-    String json = objectMapper.writeValueAsString(company);
-    System.out.println(json);
-    Company deserializedCompany = objectMapper.readValue(json, Company.class);
-    assertEquals("Andy", deserializedCompany.employees.get(0).name);
-    assertEquals("Mike", deserializedCompany.employees.get(1).name);
-  }
+        String json = objectMapper.writeValueAsString(company);
+        System.out.println(json);
+        Company deserializedCompany = objectMapper.readValue(json, Company.class);
+        assertEquals("Andy", deserializedCompany.employees.get(0).name);
+        assertEquals("Mike", deserializedCompany.employees.get(1).name);
+    }
 
-  public static class Company {
-    public List<Employee> employees;
-    public Employee starEmployee;
-    public Admin administrator;
-    public Worker gatekeeper;
-  }
+    public static class Company {
+        public List<Employee> employees;
+        public Employee starEmployee;
+        public Admin administrator;
+        public Worker gatekeeper;
+    }
 
-  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
-      include = JsonTypeInfo.As.EXISTING_PROPERTY,
-      property = "type",
-      visible = true)
-  @JsonSubTypes(value = {
-      @JsonSubTypes.Type(value = Admin.class, name = "ADMIN"),
-      @JsonSubTypes.Type(value = Worker.class, name = "WORKER"),
-  })
-  public static abstract class Employee {
-    public EmployeeType type;
-    public String name;
-  }
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.EXISTING_PROPERTY,
+            property = "type",
+            visible = true)
+    @JsonSubTypes(value = {
+            @JsonSubTypes.Type(value = Admin.class, name = "ADMIN"),
+            @JsonSubTypes.Type(value = Worker.class, name = "WORKER"),
+    })
+    public static abstract class Employee {
+        public EmployeeType type;
+        public String name;
+    }
 
-  public static class Admin extends Employee {
-    public int level;
-  }
+    public static class Admin extends Employee {
+        public int level;
+    }
 
-  public final static class Worker extends Employee {
-    public String location;
-  }
+    public final static class Worker extends Employee {
+        public String location;
+    }
 
-  public enum EmployeeType {
-    ADMIN, WORKER
-  }
+    public enum EmployeeType {
+        ADMIN, WORKER
+    }
 }
